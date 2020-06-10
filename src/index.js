c96d1d692c6f6a2176c0fbcb495a9e469468d8dd
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-06-10 23:52:07 +0500

    initial test

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <div>
+   Hello World
+ </div>,
   document.getElementById('root')
 );
 
